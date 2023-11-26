a40a32c8d37913c43fd09430bc02c9abaea44416
Sanjshine99 <sanubandara12639@gmail.com>
2023-11-26 06:59:03 +0530

    refactor(): rename the variable name and use final keyword

--- IOLibrary.java
+++ IOLibrary.java
@@ -12,21 +12,22 @@ import java.net.*;
 
 
 public final class IOLibrary {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     public static String getString() {
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
         do {
             try {
-                return r.readLine();
+                return reader.readLine();
             } catch (Exception e) {
             }
         } while (true);
     }
 
     public static InetAddress getIPAddress() {
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         do {
             try {
-                String[] chunks = r.readLine().split("\\.");
+                String[] chunks = reader.readLine().split("\\.");
                 byte[] data = {Byte.parseByte(chunks[0]), Byte.parseByte(chunks[1]), Byte.parseByte(chunks[2]), Byte.parseByte(chunks[3])};
                 return Inet4Address.getByAddress(data);
             } catch (Exception e) {
